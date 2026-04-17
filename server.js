ba218a4b277f2f3d43f25409fcabde145d0bbfa3
G-Kangutkar <kangutkargauri9205@gmail.com>
2026-04-17 15:35:34 +0530

    adding server

---
+++ server.js
@@ -0,0 +1,12 @@
+import express from "express";
+import todo from './routes/auth.routes.js'
+
+const app = express()
+
+app.use(express.json())
+
+app.use('/user',todo)
+
+app.listen(7900,()=>{
+    console.log('running')
+})
